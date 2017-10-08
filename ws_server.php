91bdeb376753ce495effa617794e4f4f0d6a9f51
wusong <877183659@qq.com>
2017-10-08 08:09:49 +0800

    增加弹幕

--- ws_server.php
+++ ws_server.php
@@ -1,20 +1,31 @@
 <?php
 $server = new swoole_websocket_server("0.0.0.0", 9502);
 $server->on('open', function (swoole_websocket_server $server, $request) {
-        echo "server: handshake success with fd{$request->fd}\n";
-    });
+
+});
 $server->on('message', function (swoole_websocket_server $server, $frame) {
-        echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
-        $server->push($frame->fd, "this is server");
-    });
-$server->on('close', function ($ser, $fd) {
-        echo "client {$fd} closed\n";
-    });
+    $data = json_decode($frame->data,true);
+    if($data['connect']===TRUE)
+    {
+        $msg = '<span style="color:#ccc">'.date('Y-m-d H:i:s').' '.$data['msg'].'</span>';
+    }elseif($data['connect']===FALSE)
+    {
+        $msg = ' <span style="color:red">' . $data['username'] . ' '. date('Y-m-d H:i:s') ."：</span><br>{$data['msg']}\n";
+    }elseif($data['connect'] === 'barrage') // 弹幕
+    {
+        $msg = $data['msg'];
+    }
+    foreach ($server->connections as $v) {  // 所有在线用户！
+        $server->push($v, $msg);
+    }
+});
+$server->on('close', function ($server, $fd) {
+    echo "client {$fd} closed\n";
+});
 $server->on('request', function (swoole_http_request $request, swoole_http_response $response) {
-        global $server;//调用外部的server
-        // $server->connections 遍历所有websocket连接用户的fd，给所有用户推送
-        foreach ($server->connections as $fd) {
-            $this->server->push($fd, $request->get['message']);
-        }
-    });
+    global $server;//调用外部的server
+    foreach ($server->connections as $fd) {
+        $this->server->push($fd, $request->get['message']);
+    }
+});
 $server->start();
